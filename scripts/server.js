99a626162ad8494b459578ba30434de15a49d18a
afnlite <afnlite@gmail.com>
2018-02-27 08:02:10 +0200

    Add webpack-dev-middleware

--- scripts/server.js
+++ scripts/server.js
@@ -9,6 +9,11 @@ const port = 3030;
 const app = express();
 const compiler = webpack(config);
 
+app.use(require('webpack-dev-middleware')(compiler, {
+   noInfo: true,
+   publicPath: config.output.publicPath,
+}));
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 });
